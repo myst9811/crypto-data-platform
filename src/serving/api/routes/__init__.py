5b75fb48eaca928217a9453f90c6aacd17f1eb3c
Shannen Saikia <shannen.saikia@gmail.com>
2026-02-01 14:59:54 +0530

    feat: integrating all api route modules

--- src/serving/api/routes/__init__.py
+++ src/serving/api/routes/__init__.py
@@ -1,5 +1,17 @@
 """API route modules."""
 
 from .health import router as health_router
+from .prices import router as prices_router
+from .vwap import router as vwap_router
+from .volume import router as volume_router
+from .liquidity import router as liquidity_router
+from .arbitrage import router as arbitrage_router
 
-__all__ = ["health_router"]
+__all__ = [
+    "health_router",
+    "prices_router",
+    "vwap_router",
+    "volume_router",
+    "liquidity_router",
+    "arbitrage_router",
+]
